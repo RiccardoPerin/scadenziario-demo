5c98a7cbdadc68cfa45a8aa89f0da5862ade1130
Riccardo Perin <riccardo.perin03@gmail.com>
2026-09-18 22:07:10 +0200

    Modify demo build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1386205217" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3407839381" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
